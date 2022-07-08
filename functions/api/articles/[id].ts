29e640907a9c4d22f032904ddcb38f2b313ab3f0
Sueqkjs <sueqk@outlook.jp>
2022-07-08 23:31:13 +0000

    wt

---
+++ functions/api/articles/[id].ts
@@ -0,0 +1,40 @@
+async function get({
+  apiKey,
+  domain,
+  endpoint,
+  contentId
+}: {
+  apiKey: string;
+  domain: string;
+  endpoint: string;
+  contentId?: string;
+}): Promise<Object | string> {
+  return await fetch(
+    `https://${domain}.microcms.io/api/v1/${endpoint}${contentId ? "/" + contentId : ""}`,
+    {
+      headers: {
+        "X-MICROCMS-API-KEY": apiKey
+      }
+    }
+  )
+    .then(async (r) => await r.json())
+    .catch((e) => e.message);
+}
+
+export async function onRequestGet({ params, env }) {
+  let isList = params.id === "LIST";
+
+  let res = await get({
+    apiKey: env.MICROCMS_API_KEY,
+    domain: env.DOMAIN,
+    endpoint: "articles",
+    contentId: isList ? void 0 : params.id
+  });
+
+  return new Response(JSON.stringify(res), {
+    status: res instanceof String ? 400 : 200,
+    headers: {
+      "Content-type": "application/json"
+    }
+  });
+}
--- src/routes/articles/[id].ts
+++ src/routes/articles/[id].ts
@@ -3,12 +3,11 @@ import client from "$lib/client";
 export async function get({ params }: {params: Record<string, string>}): Promise<Object> {
   const article = await client
     .get({
-      endpoint: "articles",
       contentId: params.id
     })
     .catch(console.error);
-  console.log(article);
-  if (article)
+
+    if (article)
     return {
       body: {
         article
